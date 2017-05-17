8bf09d59a0160ba541be9a1108352f34a49dbca3
MattK9567 <codename4756@gmail.com>
2017-05-17 17:19:52 +0200

    Upraveny_vzhlad_admin_template

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -11,42 +11,42 @@ class Dashboard extends MY_Controller
     function index()
     {
         $data['content_view'] = 'dashboard/dashboard1_v';
-
         $this->template->admin_template($data);
     }
 
     function dash2()
     {
         $data['content_view'] = 'dashboard/dashboard2_v';
-
         $this->template->admin_template($data);
     }
 
     function dash3()
     {
         $data['content_view'] = 'dashboard/dashboard3_v';
-
         $this->template->admin_template($data);
     }
 
     function addCustomer()
     {
         $data['content_view'] = 'zakaznici/addCustomer_v';
-        
         $this->template->admin_template($data);
     }
 
     function addSportovisko()
     {
         $data['content_view'] = 'sportoviska/addSportovisko_v';
-        
         $this->template->admin_template($data);
     }
 
     function addRezervacia()
     {
         $data['content_view'] = 'rezervacie/addRezervacia_v';
-        
+        $this->template->admin_template($data);
+    }
+
+    function domov()
+    {
+        $data['content_view'] = 'dashboard/domov_v';
         $this->template->admin_template($data);
     }
 }
